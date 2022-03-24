ef5d3d3277cd2884de0e372b5b2067279c5b307f
Dio Lemos <pedrodiogenes@outlook.com>
2022-03-24 12:31:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 let gameBoard = {
-    cel1: document.getElementById(cel1),
-    cel2: document.getElementById(cel2),
-    cel3: document.getElementById(cel3),
-    cel4: document.getElementById(cel4),
-    cel5: document.getElementById(cel5),
-    cel6: document.getElementById(cel6),
-    cel7: document.getElementById(cel7),
-    cel8: document.getElementById(cel8),
-    cel9: document.getElementById(cel9),
+    tiles: [null, null, null, null, null, null, null, null, null,]
+}
+
+
+const gameBoardArr = []
+
+for (let i = 0; i < 9; i++) {
+    gameBoardArr.push('')
 }
 
 let allCells = document.querySelectorAll('.boardcell')
