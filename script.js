3e88c400f8bbd84fb6f480eda24e28a12d623ec7
Dio Lemos <pedro.devsc@outlook.com>
2023-07-31 23:49:14 -0300

    update script

--- script.js
+++ script.js
@@ -1,71 +1,61 @@
 // 'use strict'
 
-startBtn = document.getElementById("start-btn")
+startBtn = document.getElementById("start-btn");
 
+const gameBoardModule = (() => {
+  let gameBoard = ["", "", "", "", "", "", "", "", ""];
 
+  let boardInnerHtml;
+  const renderBoard = () => {
+    boardInnerHtml = ""
+    gameBoard.forEach((cell, index) => {
+      boardInnerHtml += `<div class='cell' id='cell-${index}'>${cell}</div>`;
+    });
+    document.querySelector(".game-board").innerHTML = boardInnerHtml;
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      cell.addEventListener("click", Game.handleClick);
+    });
+  };
 
-const gameBoardModule = (()=>{
+  const update = (index, value) => {
+    gameBoard[index] = value;
+  };
 
-    let gameBoard =  ["", "", "", "", "", "", "", "", "",]
+  return { renderBoard, update };
+})();
+//player factory
+const Player = (PlayerName, marker) => {
+  return { PlayerName, marker };
+};
+
+const Game = (() => {
+  // player1Name = document.getElementById('player1')
+  // player2Name = document.getElementById('player2')
+  let players = [];
+  let currentPlayerIndex;
+  let isGameOver;
+
+  const start = () => {
+    players = [
+      Player((playerName = "player 1"), (marker = "X")),
+      Player((playerName = "player 2"), (marker = "O")),
+    ];
+    currentPlayerIndex = 0;
+    isGameOver = false;
+    gameBoardModule.renderBoard();
     
-    let boardInnerHtml = ""
-    const renderBoard = ()=>{
-       
-        gameBoard.forEach(
-            (cell,index)=>{
-            boardInnerHtml += `<div class='cell' id='cell-${index}'>${cell}</div>`
-        })
-        document.querySelector(".game-board").innerHTML = boardInnerHtml
-       
-       
-    }
-    const update = (index, value)=>{
-        gameBoard[index] = value
-        
-    }
+  };
 
-    return {renderBoard,update}
+  const handleClick = (event) => {
+    let index = parseInt(event.target.id.split("-")[1]);
+    console.log(index)
+    gameBoardModule.update(index, players[currentPlayerIndex].marker);
+    gameBoardModule.renderBoard();
+  };
+  return { start, handleClick };
+})();
 
-})()
-const Player = (PlayerName, marker)=>{
-    return{PlayerName, marker}
-}
-
-const Game = (()=>{
-    // player1Name = document.getElementById('player1')
-    // player2Name = document.getElementById('player2')
-    let players = []
-    let currentPlayerIndex
-    let isGameOver 
-
-    const start = ()=>{
-        players = [
-            Player(playerName="player 1",marker='X'),
-            Player(playerName="player 2",marker='O')
-        ]
-    currentPlayerIndex = 0   
-    isGameOver = false 
-    gameBoardModule.renderBoard( )
-    }
-    const cells = document.querySelectorAll(".cell")
-    cells.forEach((cell)=>{
-        cell.addEventListener("click", handleClick)
-    })
-    const handleClick = (event)=>{
-        let index = parseInt(event.target.id.split("-")[1])
-        gameBoardModule.update(index, players[currentPlayerIndex].marker)
-        gameBoardModule.renderBoard()
-    }
-    return {start}
-})()
-
-
-
-
-
-
-
-startBtn.addEventListener("click",(e)=>{
-    Game.start()
-    })
-    
+startBtn.addEventListener("click", (e) => {
+  Game.start();
+});
